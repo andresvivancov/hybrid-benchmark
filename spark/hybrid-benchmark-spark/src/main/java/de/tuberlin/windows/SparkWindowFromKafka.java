3d0baf4f3832af46c2f34ccd86a896d973f90283
Patrick Lehmann <pat@my-lehmann.de>
2017-02-27 19:51:08 +0100

    added results + changed printing

--- spark/hybrid-benchmark-spark/src/main/java/de/tuberlin/windows/SparkWindowFromKafka.java
+++ spark/hybrid-benchmark-spark/src/main/java/de/tuberlin/windows/SparkWindowFromKafka.java
@@ -148,7 +148,7 @@ public class SparkWindowFromKafka implements Serializable{
 
 
         //print results
-        joinedStream.print();
+        //joinedStream.print();
 
 
         //printing output
